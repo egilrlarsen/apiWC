0731b5914faefce59973de1ec1695e36609b41d0
egilrlarsen <37373384+egilrlarsen@users.noreply.github.com>
2018-12-12 08:47:36 +0100

    Models

---
+++ Models/word.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiwc.Models
+{
+    public class word
+    {
+        public string sWord { get; set; }
+        public int count { get; set; }
+
+
+ 
+
+    }
+
+
+
+   
+
+}
